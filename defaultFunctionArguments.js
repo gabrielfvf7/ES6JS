75c3ab7cd3c996f8ca3a4963f7ee5e77550e5a44
gabrielfvf7 <gabrielfvf@outlook.com>
2019-09-09 13:35:20 -0300

    defaultArguments

---
+++ defaultFunctionArguments.js
@@ -0,0 +1,9 @@
+function makeAjaxRequest(url, method = 'GET') {
+    return method 
+   }
+   
+   
+   
+   makeAjaxRequest('google.com')
+   makeAjaxRequest('google.com', null)
+   makeAjaxRequest('google.com', 'POST')
