8b11c3143671949db15e1c3db0153b1959a81f1a
gabrielfvf7 <gabrielfvf@outlook.com>
2019-09-10 12:37:28 -0300

    final classes

--- classes.js
+++ classes.js
@@ -1,13 +1,22 @@
 class Car {
-    constructor({ title })  {
-      this.title = title;
-    }
-    
-    drive() {
-      return 'vroom'
-    }
+  constructor({ title })  {
+    this.title = title;
   }
   
-  const car = new Car({ title: 'Toyota' });
-  car.drive();
-  car.title
+  drive() {
+    return 'vroom'
+  }
+}
+
+class Toyota extends Car {
+constructor(options) {
+  super(options);
+  this.color = options.color;
+}
+
+honk() {
+  return 'beep';
+}
+}
+
+const t = new Toyota({color: 'blue', title: 'carro'})
